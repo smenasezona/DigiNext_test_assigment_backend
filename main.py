9b7c01e885b8cd4e189139fa1eb1805ed286044c
Artem <artemkiasko@gmail.com>
2024-03-15 20:58:44 +0300

    fix #2

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ CORS(app)
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
-        db = g._database = sqlite3.connect('./identifier.sqlites')
+        db = g._database = sqlite3.connect('./identifier.sqlite')
     return db
 
 
